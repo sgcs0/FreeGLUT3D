2623b067a4abadf2159372e0f6288a3c1931473c
sgcs <58474573+sgcs0@users.noreply.github.com>
2025-12-04 15:29:19 +0100

    Add files via upload

--- Engine.h
+++ Engine.h
@@ -2,14 +2,24 @@
 #define ENGINE_H
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <GL/glut.h>
 #include "SimpleShapes.h"
+#include "ManualShapes.h"
+#include "ProceduralShapes.h"
 
 class Engine {
 private:
     int width, height;
-    float rotation;
-    SimpleShapes shapes;
+    float rotX, rotY, rotZ; // Z for Roll
+    float bgR, bgG, bgB;
+    int currentMode; 
+    bool orthoMode; // Lab 07: Projection toggle
+
+    SimpleShapes simpleShapes; 
+    ManualShapes manualShapes;
+    ProceduralShapes proceduralShapes;
+    
     static Engine* instance;
 
 public:
@@ -21,9 +31,13 @@ private:
     void render();
     void update();
     void reshape(int w, int h);
+    void handleMouse(int button, int state, int x, int y);
+    void handleKeyboard(unsigned char key, int x, int y);
     
     static void displayWrapper();
     static void reshapeWrapper(int w, int h);
+    static void mouseWrapper(int button, int state, int x, int y);
+    static void keyboardWrapper(unsigned char key, int x, int y);
     static void timerWrapper(int value);
 };
 
